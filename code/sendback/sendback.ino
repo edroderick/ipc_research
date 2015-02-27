d73a5016d8ee58e4db75d5a05300c9c7f58d26c4
Ed Roderick <roderickvt@gmail.com>
2015-02-27 18:53:36 -0500

    retest

--- code/sendback/sendback.ino
+++ code/sendback/sendback.ino
@@ -11,27 +11,31 @@ char inChar;
 byte index = 0;
 
 void setup() {
-  //Serial3.begin(57600);
+  Serial3.begin(9600);
 }
 
 void loop() {
  
-  //if(Serial3.available() > 0){
-  if(SerialUSB.available() > 0){
-    char inData[20];
-    //while(Serial3.available()>0){
-    while(SerialUSB.available()>0){
-      if(index < 19){
-        //inChar = Serial3.read(); //read a character
-        inChar = SerialUSB.read(); //read a character
-        inData[index] = inChar;
-        index++;
-        inData[index] = '\0';
-      }
+  //while(Serial3.available() == 0){}  //block untill serial available
+  while(SerialUSB.available() == 0){}  //block untill serial available
+  //if (Serial3.available() > 0){
+  if (SerialUSB.available() > 0){
+    //while(Serial3.available() > 0) {
+    while(SerialUSB.available() > 0) {
+      char inData[20];
+      //while(Serial3.available()>0){
+      while(SerialUSB.available()>0){
+        if(index < 19){
+          //inChar = Serial3.read(); //read a character
+          inChar = SerialUSB.read(); //read a character
+          inData[index] = inChar;
+          index++;
+          inData[index] = '\0';
+        }
+       }
+      SerialUSB.print(inData[0]);
     }
-  
   index = 0;
-  SerialUSB.print(inData[0]);
 }
 }
 
