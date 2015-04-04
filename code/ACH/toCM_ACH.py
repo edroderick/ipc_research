ce459f38ff5eb9f4c34b39122ba8279526a32a7b
Ed Roderick <roderickvt@gmail.com>
2015-04-04 17:47:58 +0000

    full ach test code

---
+++ code/ACH/toCM_ACH.py
@@ -0,0 +1,20 @@
+import ach
+import serial
+import ipc_include as ipc
+
+ser = serial.Serial('/dev/ttyAMA0', 9600)
+#ser = serial.Serial('/dev/ttyACM0', 9600)
+
+chan = ach.Channel(ipc.CONTROLLER_REF_NAME)
+chan.flush()
+message = ipc.CONTROLLER_REF()
+
+while True:
+	[status, framesize] = chan.get(message, wait=True, last=True)	
+	print message.msg
+	ser.write(message.msg)
+	#print 'write ', message.msg
+
+
+
+
