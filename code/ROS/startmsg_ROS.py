af23186b91e3ce3b0899eb6213502e8c09310786
Ed Roderick <roderickvt@gmail.com>
2015-04-20 19:25:39 -0400

    new starmsgros

--- code/ROS/startmsg_ROS.py
+++ code/ROS/startmsg_ROS.py
@@ -2,16 +2,53 @@
 
 import rospy
 from std_msgs.msg import String
+from datetime import datetime
+import serial
+import time
+import random
 
 rospy.init_node('startmsg', anonymous=True)
 pub = rospy.Publisher('link1', String, queue_size=10)
+rate = rospy.Rate(10)
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2) #USB Serial for response
+ser.flushInput()
+ser.flushOutput()
 
 def talker():
-    while not rospy.is_shutdown():
-        pub.publish('x')
-        rospy.loginfo('x')
+    lastID = 0
 
+    #test message code
+    testID = 'X'
+    while(lastID != testID):
+	print 'sending testid'
+	pub.publish(testID)
+	lastID = ser.read(1)
+
+
+
+    for i in range (1, 11):
+	uID = str(random.randint(0,9))
+	while(uID == lastID):
+		uID = str(random.randint(0,9))
+
+	tick = datetime.now()
+	pub.publish(uID)
+	msg = ser.read(1)
+	tock = datetime.now()
+	if (msg == uID):
+		dT = tock-tick
+	else:
+		dT = 'missed'
+
+	lastID = uID
+	print i, '\t', tick, '\t', tock, '\t', uID, '\t', dT
+	rate.sleep()
+	
 if __name__ == '__main__':
+    #output file header
+    print 'Run', '\t', 'T Sent', '\t', 'T Received', '\t', 'Message ID', '\t', 'dT'
+
     try:
         talker()
     except rospy.ROSInterruptException:
